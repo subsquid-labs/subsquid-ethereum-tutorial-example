9a0f3b3fb2a47a55368f6ae92f4895bca433b3ce
Massimo Luraschi <6950554+RaekwonIII@users.noreply.github.com>
2022-10-26 05:18:11 -0700

    Initial commit

---
+++ src/processor.ts
@@ -0,0 +1,23 @@
+import { Store, TypeormDatabase } from "@subsquid/typeorm-store";
+import {EvmBatchProcessor} from '@subsquid/evm-processor'
+
+const processor = new EvmBatchProcessor()
+  .setDataSource({
+    chain: process.env.ETHEREUM_MAINNET_WSS,
+    archive: 'https://eth-test.archive.subsquid.io',
+  })
+  .addLog('0x0000000000000000000000000000000000000000', {
+    filter: [[ ]],
+    data: {
+        evmLog: {
+            topics: true,
+            data: true,
+        },
+    } as const,
+});
+
+
+processor.run(new TypeormDatabase(), async (ctx) => {
+    ctx.log.info(ctx.blocks, "Got blocks")
+});
+
